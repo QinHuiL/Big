ef68264281fcba0824931f5ac2070cb70800ee4d
QinHuiL <33859282+QinHuiL@users.noreply.github.com>
2017-11-22 16:04:01 +0800

    1
    1

--- test_01.py
+++ test_01.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 from selenium import webdriver
-import selenium.webdriver.chrome.service as service
+from time import sleep
 
-service = service.Service('/usr/local/Cellar/chromedriver/2.33/bin')
-service.start()
-capabilities = {'chrome.binary': '/Applications/Google Chrome.app/Contents/MacOS'}
-driver = webdriver.Remote(service.service_url, capabilities)
+driver = webdriver.Chrome()
 driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
+sleep(5) # Let the user actually see something!
 driver.quit()
