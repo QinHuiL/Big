910f878fe004645ada9d1129c0522e6650f3dfe2
QinHuiL <33859282+QinHuiL@users.noreply.github.com>
2017-11-22 15:46:53 +0800

    1
    1

--- test_01.py
+++ test_01.py
@@ -2,7 +2,8 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(chromedriver)
+chromedriver = "/usr/local/Cellar/chromedriver/2.33/bin"
 driver.get("https://www.baidu.com")
 sleep(3)
 driver.quiet()
