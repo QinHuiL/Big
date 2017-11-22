5f490fa23163901d78132adf72b569acdb70ed4e
QinHuiL <33859282+QinHuiL@users.noreply.github.com>
2017-11-22 17:57:56 +0800

    1
    1

--- test_01.py
+++ test_01.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get('http://www.google.com/xhtml');
 sleep(5) # Let the user actually see something!
 driver.quit()
